5b80d7a436dd4d97e2d48dedb78f690160768310
auzunska <auzunska@abv.bg>
2024-03-18 23:55:24 +0200

    Lab 1

---
+++ Welcome/Model/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Welcome.Others;
+
+namespace Welcome.Model
+{
+    internal class User
+    {
+        private string _names;
+        private string _password;
+        private UserRolesEnum _role;
+        private string _email;
+        private string _facNum;
+
+        public string Name
+        {
+            get { return _names; }
+            set { _names = value; }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+
+        public UserRolesEnum Role
+        {
+            get { return _role; }   
+            set { _role = value; }
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        public string FacNum
+        {
+            get { return _facNum; }
+            set { _facNum = value; }
+        }
+    }
+}
---
+++ Welcome/Program.cs
@@ -0,0 +1,26 @@
+﻿using Welcome.Model;
+using Welcome.View;
+using Welcome.ViewModel;
+
+namespace Welcome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            user.Name = "Test";
+            user.Password = "Test";
+            user.Role = Others.UserRolesEnum.ADMIN;
+            user.Email = "test@example.com";
+            user.FacNum = "123456789";
+
+            UserViewModel viewModel = new UserViewModel(user);
+            UserView view = new UserView(viewModel);
+
+            view.DisplayNameRole();
+            view.DisplayInfo();
+            view.displayLoginCredentials();
+        }
+    }
+}
