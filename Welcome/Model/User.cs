d5d6fddbf6ce714e4c93205e49c9b1eaf33b56e2
Asya Uzunska <92046154+auzunska@users.noreply.github.com>
2024-03-18 23:59:18 +0200

    Update User.cs

--- Welcome/Model/User.cs
+++ Welcome/Model/User.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,33 +10,33 @@ namespace Welcome.Model
     internal class User
     {
         private string _names;
-        private string _password;
-        private UserRolesEnum _role;
         private string _email;
+        private int _password;
+        private UserRolesEnum _role;
         private string _facNum;
 
-        public string Name
-        {
-            get { return _names; }
+        public string Names { 
+            get { return _names; } 
             set { _names = value; }
+        }
+
+        public string Email { 
+            get { return _email; } 
+            set { _email = value; } 
         }
 
         public string Password
         {
-            get { return _password; }
-            set { _password = value; }
+            get { return _password.ToString(); }
+            set { int hash = value.GetHashCode();
+                  _password = hash;
+            }
         }
 
         public UserRolesEnum Role
         {
-            get { return _role; }   
+            get { return _role; }
             set { _role = value; }
-        }
-
-        public string Email
-        {
-            get { return _email; }
-            set { _email = value; }
         }
 
         public string FacNum
